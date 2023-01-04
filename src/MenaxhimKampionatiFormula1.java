d9c8d985aa3d17f7de740a09dda4f2e39a4a1b8f
School <luis.kateshi@fti.edu.al>
2023-01-04 17:05:21 +0100

    Created class Garat

--- src/MenaxhimKampionatiFormula1.java
+++ src/MenaxhimKampionatiFormula1.java
@@ -1,16 +1,36 @@
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.util.*;
+
+
 public class MenaxhimKampionatiFormula1 implements MenaxhimKampionati {
     int nrShofereve;
     int nrSkuadrave;
-    void ShfaqMenune(){
+    private static Scanner sc = new Scanner(System.in);
+    ArrayList<Gara> races = new ArrayList<>();
+    public static void main(String[] args) {
+
+        ShfaqMenune();
+
+    }
+    static void ShfaqMenune(){
         System.out.println("++++++++++++++++++++++ Formula1 App Menu ++++++++++++++++++++++");
         System.out.println("Zgjidhni nje nga opsionet e meposhtme duke dhene nr. perkates:");
         System.out.println("1-    Shto nje shofer te ri");
         System.out.println("2-    Perditeso shoferin per nje skuader ekzistuese");
         System.out.println("2-    Fshi shoferin dhe skuadren e tij");
     }
-    public static void main(String[] args) {
-        MenaxhimKampionatiFormula1 main = new MenaxhimKampionatiFormula1();
-        main.ShfaqMenune();
+    void addToFile() throws Exception{
+        Gara g = new Gara("04/01/2023", "Luis Hamilton", "Mercedes Benz", 3);
+        races.add(g);
+
+        new FileOutputStream("myCars.txt", true).close();
+        FileOutputStream fos = new FileOutputStream("myCars.txt");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        for (Gara race : races) {
+            oos.writeObject(race);
+        }
+        fos.close();
     }
 
 }
