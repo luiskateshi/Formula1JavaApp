134bd1fe615830a07a4d418411c49b1500cba51b
floribica <Flori.Bica@fti.edu.al>
2023-01-18 11:02:30 +0100

    Added some of the methods in class shofer.

--- src/Shofer.java
+++ src/Shofer.java
@@ -1,5 +1,4 @@
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Shofer implements Serializable {
@@ -15,14 +14,29 @@ public class Shofer implements Serializable {
         this.emriSkuadres = emriSkuadres;
     };
 
-    public static void addShofer(ArrayList<Shofer> shoferet, String emri, String mbiemri, String emriSkuadres){
+    public static void addShofer(String emri, String mbiemri, String emriSkuadres){
         Shofer s = new Shofer( emri, mbiemri, emriSkuadres);
         shoferet.add(s);
     }
 
-    public static void removeShofer( String emri) {
+    public static void removeShofer( String e) {
 
-    //    Shofer.remove(emri);
+        shoferet.remove(e);
+    }
+
+    public static void renameShofer( String e) {
+        Scanner input = new Scanner(System.in);
+        removeShofer(e);
+
+        System.out.println("emrin e ri: ");
+        String emri = input.next();
+        System.out.println("mbiemri e ri: ");
+        String mbiemri = input.next();
+        System.out.println("emrin skuadres e ri: ");
+        String emriSkuadres = input.next();
+
+        addShofer(emri,mbiemri,emriSkuadres);
+
     }
 
 
