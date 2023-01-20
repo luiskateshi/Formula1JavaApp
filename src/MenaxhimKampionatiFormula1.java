9d21a8d7a366ba6f3a96569250e2539e799377ac
School <luis.kateshi@fti.edu.al>
2023-01-20 18:09:41 +0100

    Created class Garat

--- src/MenaxhimKampionatiFormula1.java
+++ src/MenaxhimKampionatiFormula1.java
@@ -10,39 +10,60 @@ public class MenaxhimKampionatiFormula1 implements MenaxhimKampionati {
     int nrShofereve;
     int nrSkuadrave;
     private static Scanner sc = new Scanner(System.in);
-    //ArrayList<Gara> races = new ArrayList<>();
+    ArrayList<Gara> races = new ArrayList<>();
+    ArrayList<Formula1Shofer> drivers = new ArrayList<>();
     public static void main(String[] args) {
 
+        MenaxhimKampionatiFormula1 m = new MenaxhimKampionatiFormula1();
+        m.LoadData();
+
+        ShfaqMenune();
+
+
+        m.SaveData();
+
+        int nrShofereve;
+        int nrSkuadrave;
+
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Gara> races = new ArrayList<>();
 //        Test per klasen Formula1Shofer
-        Formula1Shofer.fillArrayList();
-        Formula1Shofer.ShfaqStatistikaTeGjitheShoferet();
-//    Formula1Shofer.ShfaqStatistikaShoferi("seb");
+            Formula1Shofer.fillArrayList();
+            Formula1Shofer.ShfaqStatistikaTeGjitheShoferet();
 
-//        ShfaqMenune();
-//        MenaxhimKampionatiFormula1 m = new MenaxhimKampionatiFormula1();
-//
-//        ArrayList<Gara> races = new ArrayList<>();
-//        m.LoadData(races);
-//
-//        m.SaveData(races);
+        }
 
-        //Read object from file
-
-
-    }
     static void ShfaqMenune(){
-        System.out.println("++++++++++++++++++++++ Formula1 App Menu ++++++++++++++++++++++");
-        System.out.println("Zgjidhni nje nga opsionet e meposhtme duke dhene nr. perkates:");
+
+        System.out.println("Zgjidhni nje nga funksionalitetet e meposhtme nepermjet karaktereve ne te majte:");
         System.out.println("1-    Shto nje shofer te ri");
         System.out.println("2-    Perditeso shoferin per nje skuader ekzistuese");
         System.out.println("3-    Fshi shoferin dhe skuadren e tij");
-        System.out.println("4-    Listo te gjithe shoferat");
-        System.out.println("4-    Save data");
-        System.out.println("4-    Exit");
-
+        System.out.println("4-    Shfaq Statistikat per nje shofer");
+        System.out.println("5-    Shfaq Statistikat per shoferet (Actual points DESC)");
+        System.out.println("6-    Shto nje gare te re");
+        System.out.println("s-    Save data");
+        System.out.println("x-    Exit");
+        System.out.println("\nFunksionalitetet me nderfaqe grafike (GUI):");
+        System.out.println("7-    Shfaq ");
+    }
+    void doMenu(char komanda){
+        switch (komanda) {
+            case '1':
+                //therritja e funksionit perkates
+                break;
+            case '6':
+                Gara.addRace(races, drivers);
+                break;
+            default:
+                System.out.println("Funksioni i kerkuar nuk ekziston, provoni perseri!");
+        }
     }
 
-    public void LoadData(ArrayList<Gara> races){
+
+    public void LoadData(){
+
+        //Leximi i File-it "garat.txt" dhe vendosja e informacionit ne objekt-vektorin ArrayList<Gara> races
         try {
             FileReader fr = new FileReader(file2);
             BufferedReader input = new BufferedReader(fr);
@@ -51,7 +72,7 @@ public class MenaxhimKampionatiFormula1 implements MenaxhimKampionati {
             {
                 String[] info = null;
                 info = line.split("\\t");
-                Gara.addRace(races, info[0].toString(), info[1].toString(), info[2].toString(), parseInt(info[3]));
+                races.add(new Gara(info[0].toString(), info[1].toString(), info[2].toString(), parseInt(info[3])));
             }
             input.close();
         }
@@ -60,9 +81,13 @@ public class MenaxhimKampionatiFormula1 implements MenaxhimKampionati {
             e.printStackTrace();
         }
         System.out.println("Infos u lexuan me sukses nga file.");
+
     }
-    public void SaveData(ArrayList<Gara> races){
+    public void SaveData(){
+
+        //Vendosja e cdo informacioni te futur nga user-i deri ne kete moment nga objekt-vektori ArrayList<Gara> races ne file-in "garat.txt"
         try {
+
             FileWriter fw = new FileWriter(file2);
             Writer output = new BufferedWriter(fw);
 
